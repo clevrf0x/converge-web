85fe1ac3b1c1fbde5ce2385b3b2bc3932e5e6e8e
clevrf0x <favasm72@gmail.com>
2023-07-08 16:06:20 +0530

    Added App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './pages/layout/appLayout';
+import AuthLayout from './pages/layout/authLayout';
+import Home from './pages/home';
+import Login from './pages/auth/login';
+import PageNotFound from './pages/errors/404';
+import { useState } from 'react';
 
+function App() {
+  const [ authenticated, setAuthenticated ] = useState(false);
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        {/* Authenticated user paths */}
+        {authenticated && (
+          <Routes>
+            <Route path='/' element={<AppLayout />}>
+              <Route index={true} element={<Home />} />
+              <Route path='*' element={<PageNotFound />} />
+            </Route>
+          </Routes>
+        )}
+
+        {/* Unauthenticated user paths */}
+        {!authenticated && (
+          <Routes>
+            <Route path='/' element={<AuthLayout />}>
+              <Route index={true} element={<Login />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='*' element={<PageNotFound />} />
+            </Route>
+          </Routes>
+        )}
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/errors/404.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>ERROR 404</h1>
+      <p>We can't find the page you are looking for</p>
+    </div>
+  );
+}
